be08bbdc1f3f8301aeb6864f10ab3835fb8d9af5
William Leon <weleonm@gmail.com>
2019-10-04 00:26:15 -0500

    Create base project

---
+++ src/main/java/com/test/logger/LoggerTestApplication.java
@@ -0,0 +1,16 @@
+package com.test.logger;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="weleonm@gmail.com">William Leon</a>
+ * @version 1.0
+ * @since 1.0
+ */
+@SpringBootApplication
+public class LoggerTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoggerTestApplication.class, args);
+  }
+}
